b99d4b3d37163d91f64ee901ca45f2ef7dca7d3e
sajidkhan78666 <131393212+sajidkhan78666@users.noreply.github.com>
2023-05-23 00:46:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -47,10 +47,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'SANDARU-YT'
+global.botname = 'sajidkhan'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'sticker' 
+global.author = '@sajid' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
